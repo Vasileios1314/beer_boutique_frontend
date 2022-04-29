25ef5cb294f4f3e54fd496817582a9523de05fb4
Vasileios1314 <vasilis.bobonis@hotmail.com>
2022-04-29 14:48:13 +0200

    deploy1

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,2 @@
-export const apiUrl =
-  process.env.API_URL || "https://beerboutique.netlify.app/";
+export const apiUrl = process.env.API_URL || "https://beerboutique.netlify.app";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
